49929ccecf74ba5e95c1e6347c57b96986c61b35
rodrigocardenas <rodrigocardplaz_12@offsideclub.com>
2026-05-19 23:02:05 +0200

    Instalación de Hello Elementor, Elementor y plugins base de rendimiento, SEO y seguridad

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1779829135;s:7:"authKey";s:64:"d{?~<b- V, 0J%t>OwJlYQyDKw}ZEr$MhXL?^-8c#Qk;-gu6Fu6D2wm`*VYHd-dh";s:7:"version";s:5:"1.1.0";}
